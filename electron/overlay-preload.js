886ec3925e5429acad05d52bd6e601aae7772f6a
Kiennitech <kiennitech@gmail.com>
2026-04-19 21:41:30 +0700

    Revert "feat: initial commit v0.0.1"
    This reverts commit 24fdf42bc40bb403fb44decb21396488f70d14b0.

--- electron/overlay-preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("overlayAPI", {
-  onData: (callback) => ipcRenderer.on("overlay:data", (_e, data) => callback(data)),
-  onSettings: (callback) => ipcRenderer.on("overlay:settings", (_e, data) => callback(data)),
-  close: () => ipcRenderer.send("overlay:close"),
-  dragStart: () => ipcRenderer.send("overlay:drag-start"),
-  dragMove: (dx, dy) => ipcRenderer.send("overlay:drag-move", dx, dy),
-});
